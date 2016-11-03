f16336989e72cb8e451056edaab1e6002fef1544
aeoleader <aeoleader@gmail.com>
2016-11-03 15:35:28 +0800

    Initial Commit

---
+++ GPU-path-tracing-tutorial-4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GPU-path-tracing-tutorial-4
+//
+//  Created by jxkj on 03/11/2016.
+//  Copyright © 2016 xiangyu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
